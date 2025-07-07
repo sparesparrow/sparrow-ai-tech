57fc3fb3c36d6808d4f785d3c45f9788925e817a
sparesparrow <dallheimal@gmail.com>
2025-07-07 16:13:25 +0200

    chore: aktualizace konfigurace Cypress pro místní a produkční prostředí, přidání nových závislostí do package.json a package-lock.json, úprava GitHub Actions workflow pro nasazení statického obsahu

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173/", // For local dev
-    // For production E2E, use:
+    baseUrl: "http://localhost:5173", // For local dev
+    // For production E2E, use:edddddddxc
     // baseUrl: "https://sparesparrow.github.io/sparrow-ai-tech/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
