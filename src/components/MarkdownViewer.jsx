2bd5033f9b3e87772d9035d6d3cc724b2d09e21b
sparesparrow <dallheimal@gmail.com>
2025-07-09 14:40:47 +0200

    refactor: přechod z jest.config.cjs na jest.config.mjs; odstranění zastaralého souboru a aktualizace příkazu pro spuštění testů v package.json; přidání testů pro komponentu MarkdownViewer

--- src/components/MarkdownViewer.jsx
+++ src/components/MarkdownViewer.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import MermaidPreviewLink from './MermaidPreviewLink';
